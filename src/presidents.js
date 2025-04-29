3cf83baf4170aaa2b46f872d5af3bf0ea2e1577b
Gaelle <gaelle.madelaine@gmail.com>
2025-04-29 19:59:07 +0200

    Iteracion 4 sin resolver

--- src/presidents.js
+++ src/presidents.js
@@ -419,25 +419,56 @@ const presidents = [
 
 
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
+function getNames(presidentsArr) {
+  const presidentNames = presidentsArr.map(function (president){
+    return president.name;
+  })
+  console.log(presidentNames)
+  return presidentNames
+} 
+
 
 
 
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
+function getDemocraticPresidents(presidentsArr) {
+  
+  let Democratepresident = presidentsArr.filter((cadapresidente) => {
+    if (cadapresidente.party === "Democratic") {
+      return true
+    } else {
+      return false
+    }
+  })
+  return Democratepresident
+  }
+
 
 
 
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
+function  countYearsInOffice(presidentsArr) {
+      const totalYears = presidentsArr.reduce(function(acc, president) {
+        if ( president.leftOffice === null) {
+          return acc
+            } 
+           return acc + (president.leftOffice - president.tookOffice) ;   
+      }, 0)
+    return totalYears
+    }
+      
 
 
 
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
+function sortPresidentsByBirthYear(presidentsArr) {
+function sortedbyAge (Years1, Years2) {
+  return Years1.birthYear -Years2.birthYear;
+}
+presidentsArr.sort(sortedbyAge)}
 
 
 
